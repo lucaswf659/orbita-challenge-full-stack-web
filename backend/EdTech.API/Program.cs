6fc158ea03920d3b1911b22e7e89bd7ec56e1d33
Lucas Weber <lucaswf659@gmail.com>
2025-05-24 18:00:53 -0300

    config: solving CORS problem

--- backend/EdTech.API/Program.cs
+++ backend/EdTech.API/Program.cs
@@ -1,19 +1,29 @@
 using EdTech.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddControllers(); // Adiciona suporte aos Controllers
+builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                          policy.WithOrigins("http://localhost:5173")
+                                .AllowAnyHeader()
+                                .AllowAnyMethod();
+                      });
+});
 
 var app = builder.Build();
 
-// Middleware
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -21,7 +31,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseCors(MyAllowSpecificOrigins);
 
-app.MapControllers(); // Mapeia os Controllers (como o StudentsController)
+app.MapControllers(); 
 
 app.Run();
